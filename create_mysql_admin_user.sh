ab1dc5ed0eac733ef0204274b6803505d2aa6cf5
Amit Agarwal <raj77in@users.noreply.github.com>
2020-12-27 13:42:49 +0000

    Update create_mysql_admin_user.sh
    Fixed issue with mysqld not starting up automatically

--- create_mysql_admin_user.sh
+++ create_mysql_admin_user.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
 nohup /usr/bin/mysqld_safe > /dev/null 2>&1 &
-
-RET=1
-while [[ $RET -ne 0 ]]; do
-    echo "=> Waiting for confirmation of MySQL service startup"
-    sleep 5
-    mysql -uroot -e "status" > /dev/null 2>&1
-    RET=$?
-done
-
-PASS=${MYSQL_PASS:-PPAAssWW00RRdd}
+sleep 3
+PASS=${MYSQL_PASS}
 
 echo "=> Creating MySQL admin user with ${PASS} password"
 
@@ -31,7 +23,7 @@ echo "Please remember to change the above password as soon as possible!"
 echo "MySQL user 'root' has no password but only allows local connections"
 echo "========================================================================"
 
-mysqladmin -uroot shutdown
+#mysqladmin -uroot shutdown
 sed -i 's/static public $mMySQLDatabasePassword =.*/static public $mMySQLDatabasePassword = \"'$PASS'\";/g' /var/www/html/mutillidae/classes/MySQLHandler.php
 sed -i 's/p@ssw0rd/'$PASS'/g' /var/www/html/dvwa/DVWA-master/config/config.inc.php
 
